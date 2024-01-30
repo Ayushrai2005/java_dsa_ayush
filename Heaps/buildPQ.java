54ad5b999452add16df15d5b16a2a32e72d1932b
Ayushrai2005 <shweetarai@gmail.com>
2024-01-30 22:40:46 +0530

    Learning heaps

---
+++ Heaps/buildPQ.java
@@ -0,0 +1,28 @@
+import java.util.PriorityQueue;
+
+public class buildPQ{
+
+
+    public static void main(String[] args) {
+
+        int sorted[]  = new int[50];
+        
+
+        PriorityQueue<Integer> pq = new PriorityQueue<>();
+
+        pq.add(3); 
+        pq.add(4);
+        pq.add(1);
+        pq.add(7);
+
+        while(!pq.isEmpty()){
+
+        pq.remove();
+        }
+        System.out.println();
+
+
+
+
+    }
+}
